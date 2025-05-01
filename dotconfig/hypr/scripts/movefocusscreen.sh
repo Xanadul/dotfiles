89ea1c18ba8ef7db727e1632c93be5161726d5c2
Xanadul <xanadul@protonmail.com>
2025-05-02 00:19:36 +0200

    Hyprcleanup, some small stuff

--- dotconfig/hypr/scripts/movefocusscreen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NowWindow="$(hyprctl activewindow -j | jq ".address")"
-
-hyprctl dispatch hy3:movefocus "$1"  # && sleep 0.05
-ThenWindow="$(hyprctl activewindow -j | jq ".address")"
-if [ "$NowWindow" == "$ThenWindow" ]; then
-  hyprctl dispatch movefocus "$1"
-fi
